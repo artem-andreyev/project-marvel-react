2176772a8ebeb9f413989392b078d1cfc63c62b6
Artem <artem.andreyev212@gmail.com>
2024-07-17 15:58:03 +0300

    Working with the skeleton side and character list on click functions

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,6 +24,7 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             desciption: char.desciption ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
