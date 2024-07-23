28a19e12ace243c0b3eb3a2bf47db6db0af3be08
Artem <artem.andreyev212@gmail.com>
2024-07-23 21:40:32 +0300

    Add ref to characters in the character list

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -27,8 +27,8 @@ class CharList extends Component {
     onRequest = (offset) => {
         this.onCharListLoading();
         this.marvelService.getAllCharacters(offset)
-        .then(this.onCharListLoaded)
-        .catch(this.onError)
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
     }
 
     onCharListLoading = () => {
@@ -59,18 +59,35 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     renderItems(arr) {
-        const items =  arr.map((item) => {  
+        const items =  arr.map((item, i) => {  
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
             }
             
             return (
-                <li 
+                <li
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
