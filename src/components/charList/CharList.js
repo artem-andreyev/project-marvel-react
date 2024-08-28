a1b843aea8df07daa625acb053ca1cb8f94a6546
Artem <artem.andreyev212@gmail.com>
2024-08-28 22:24:49 +0300

    Add transition (animation) for characters on the main page when they are loaded

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
 import {useState, useEffect, useRef} from 'react';
 import PropTypes from "prop-types";
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from "../spinner/Spinner";
@@ -55,11 +56,14 @@ const CharList = (props) => {
             }
             
             return (
-                <li
+                <CSSTransition
+                key={item.id}
+                timeout={500}
+                classNames="char__item">
+                    <li
                     className="char__item"
                     tabIndex={0}
                     ref={el => itemRefs.current[i] = el}
-                    key={item.id}
                     onClick={() => {
                         props.onCharSelected(item.id);
                         focusOnItem(i);
@@ -73,12 +77,15 @@ const CharList = (props) => {
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
+                </CSSTransition>
             )
         });
 
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
