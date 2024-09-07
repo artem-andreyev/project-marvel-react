c772d9c9429a08905f9daa6b8a6fabf8fea0a11f
Artem <artem.andreyev212@gmail.com>
2024-09-07 18:42:26 +0300

    Continue working on the character search panel

--- src/components/charSeachForm/CharSearchForm.js
+++ src/components/charSeachForm/CharSearchForm.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Formik, Form, Field, ErrorMessage as FormikErrorMessage, useField} from "formik";
+import {Formik, Form, Field, ErrorMessage as FormikErrorMessage} from "formik";
 import * as Yup from "yup";
 import {Link} from "react-router-dom";
 
@@ -23,25 +23,52 @@ const CharSearchForm = () => {
             .then(onCharLoaded);
     }
 
+    const errorMessage = error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
+    const results = !char ? null : char.length > 0 ?
+                    <div className="char__search-wrapper">
+                        <div className="char__search-success">There is! Visit {char[0].name} page?</div>
+                        <Link to={`/characters/${char[0].id}`} className="button button__secondary">
+                            <div className="inner">To page</div>
+                        </Link>
+                    </div> : 
+                    <div className="char__search-error">
+                        The character was not found. Check the name and try again
+                    </div>;
+
     return (
-        <Formik
-        initialValues ={{
-            name: ""
-        }}
-        validationSchema = {Yup.object({
-            name: Yup.string()
-                    .required("This field is required")
-        })}
-        onSubmit = {values => console.log(JSON.stringify(values, null, 2))}
-    >
-        <Form className='form'>
-            <h2>Or find a character by name: </h2>
-            <input
-                label="Enter name"
-                name="name"
-        />
-        </Form>
-    </Formik>
+        <div className="char__search-form">
+            <Formik
+                initialValues = {{
+                    charName: ''
+                }}
+                validationSchema = {Yup.object({
+                    charName: Yup.string().required('This field is required')
+                })}
+                onSubmit = { ({charName}) => {
+                    updateChar(charName);
+                }}
+            >
+                <Form>
+                    <label className="char__search-label" htmlFor="charName">Or find a character by name:</label>
+                    <div className="char__search-wrapper">
+                        <Field 
+                            id="charName" 
+                            name='charName' 
+                            type='text' 
+                            placeholder="Enter name"/>
+                        <button 
+                            type='submit' 
+                            className="button button__main"
+                            disabled={loading}>
+                            <div className="inner">find</div>
+                        </button>
+                    </div>
+                    <FormikErrorMessage component="div" className="char__search-error" name="charName" />
+                </Form>
+            </Formik>
+            {results}
+            {errorMessage}
+        </div>
     )
 }
 
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,9 +8,16 @@ const useMarvelService = () => {
     const _baseOffset = 210;
 
     const getAllCharacters = async (offset = _baseOffset) => {
-        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
-        return res.data.results.map(_transformCharacter);
-    }
+		const res = await request(
+			`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(_transformCharacter);
+	};
+    
+    const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
 
     const getAllComics = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
@@ -51,7 +58,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
+    return {loading, error, clearError, getAllCharacters, getCharacter, getCharacterByName, getAllComics, getComic}
 }
 
 export default useMarvelService;
