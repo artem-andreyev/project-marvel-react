e5280fbebd6deb366ff52e5f737a8a87fcb3203b
Artem <artem.andreyev212@gmail.com>
2024-09-11 21:44:20 +0300

    Started working with the FSM (Finite-state machine)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import {useHttp} from "../hooks/htttp.hook";
 
 const useMarvelService = () => {
-    const {loading, request, error, clearError} = useHttp();
+    const {loading, request, error, clearError, process} = useHttp();
 
     const _apiBase = "https://gateway.marvel.com:443/v1/public/";
     const _apiKey = "apikey=87d7f479c0a58d97ca140b36ac567f64";
@@ -58,7 +58,15 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getCharacterByName, getAllComics, getComic}
+    return {loading, 
+            error, 
+            clearError, 
+            process, 
+            getAllCharacters, 
+            getCharacter, 
+            getCharacterByName, 
+            getAllComics, 
+            getComic}
 }
 
 export default useMarvelService;
